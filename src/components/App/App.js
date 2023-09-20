8c5ca7b51d701ee12cedb184ba87300073c5c758
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-09-20 16:44:20 -0500

    Adjust data to include id

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,15 +4,15 @@ import Ideas from "../Ideas/Ideas";
 
 const App = () => {
   const [ideas, setIdeas] = useState([
-    {title: "Bike", desc: "I want to ride my bicycle, I want to ride my bike."},
-    {title: "Thirsty or Sad?", desc: "Try drinking a full glass of water before giving up on yourself."},
-    {title: "Hot Girl Walk", desc: "Doesn't matter how long, just get out there <3"}
+    {id: 1, title: "Bike", desc: "I want to ride my bicycle, I want to ride my bike."},
+    {id: 2, title: "Thirsty or Sad?", desc: "Try drinking a full glass of water before giving up on yourself."},
+    {id: 3, title: "Hot Girl Walk", desc: "Doesn't matter how long, just get out there <3"}
   ])
  
     return (
       <div className="app-container">
         <h1>I have an Idea 💡</h1>
-        <Ideas />
+        <Ideas ideas={ideas}/>
       </div>
     )
 }
