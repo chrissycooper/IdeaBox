3df2cb1711f641d961d923eb03c8e0dcd206ac33
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-09-20 16:47:20 -0500

    Add prop drilling to pass data through the tree

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Card.css";
 
-const Card = () => {
+const Card = ({title, desc, id}) => {
 
   return (
     <div className="card">
-      <h3>I am card</h3>
-      <p> this is my description</p>
+      <h3>{title}</h3>
+      <p> {desc}</p>
     </div>
   )
 }
--- src/components/Ideas/Ideas.js
+++ src/components/Ideas/Ideas.js
@@ -2,13 +2,17 @@ import React from "react";
 import "./Ideas.css";
 import Card from "../Card/Card";
 
-const Ideas = () => {
+const Ideas = ({ideas}) => {
+  console.log(ideas)
+  const ideaMap = ideas.map((idea) => {
+    return <Card title={idea.title} desc={idea.desc} key={idea.id}/> 
+  })
 
   return (
     <>
       <h2> Here are my Ideas </h2>
       <div className="ideas-container">
-        <Card />
+        {ideaMap}
       </div>
     </>
   )
