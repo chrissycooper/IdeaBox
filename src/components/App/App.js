bc3ec9ddd06b6a774ed15df7d691b27b24e9eb96
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-09-20 16:09:32 -0500

    Set up basic App component with ideas state

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [ideas, setIdeas] = useState([])
+ 
+    return (
+      <h1>I have an Idea 💡</h1>
+    )
+}
+
+export default App;
