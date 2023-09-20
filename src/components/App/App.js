2ceedcaedd3c651ab0ea82f3e7a364d56a4b9a11
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-09-20 16:17:24 -0500

    Set up basic Ideas component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
+import "./App.css";
+import Ideas from "../Ideas/Ideas";
 
 const App = () => {
   const [ideas, setIdeas] = useState([])
  
     return (
-      <h1>I have an Idea 💡</h1>
+      <div className="app-container">
+        <h1>I have an Idea 💡</h1>
+        <Ideas />
+      </div>
     )
 }
 
---
+++ src/components/Ideas/Ideas.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Ideas.css";
+
+const Ideas = () => {
+
+  return (
+    <>
+      <h2> Here are my Ideas </h2>
+    </>
+  )
+}
+
+export default Ideas;
