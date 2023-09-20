1f50fcb7abadbbf63236403bd32458beeb4a283d
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-09-20 16:27:49 -0500

    Set up basic Card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Card.css";
+
+const Card = () => {
+
+  return (
+    <div>
+      <h3>I am card</h3>
+      <p> this is my description</p>
+    </div>
+  )
+}
+
+export default Card;
--- src/components/Ideas/Ideas.js
+++ src/components/Ideas/Ideas.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Ideas.css";
+import Card from "../Card/Card";
 
 const Ideas = () => {
 
   return (
     <>
       <h2> Here are my Ideas </h2>
+      <Card />
     </>
   )
 }
