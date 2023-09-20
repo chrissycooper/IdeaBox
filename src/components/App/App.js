e7efa39ba78d3ebcc5836259fb5d4b788d78f279
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-09-20 16:41:04 -0500

    Add basic data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,11 @@ import "./App.css";
 import Ideas from "../Ideas/Ideas";
 
 const App = () => {
-  const [ideas, setIdeas] = useState([])
+  const [ideas, setIdeas] = useState([
+    {title: "Bike", desc: "I want to ride my bicycle, I want to ride my bike."},
+    {title: "Thirsty or Sad?", desc: "Try drinking a full glass of water before giving up on yourself."},
+    {title: "Hot Girl Walk", desc: "Doesn't matter how long, just get out there <3"}
+  ])
  
     return (
       <div className="app-container">
